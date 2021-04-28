743a37fede9155bd4e6a5e8114bf27c3b9b76e18
jgumerove <josh.gumerove@gmail.com>
2021-04-28 16:33:11 -0400

    commit tree ac56b9625ab0d7dc4c9f826900ef079a77380377

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Josh Gumerov"
+    end
+
+    get '/hometown' do
+        "My hometown is Los Angeles"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is anything country"
+    end
 end
